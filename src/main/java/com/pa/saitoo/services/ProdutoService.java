099862f04c2ea6d4af9414126ff33c28e0b30a6d
unknown <helo.saito@outlook.com>
2026-03-16 19:27:16 -0300

    Service criada

--- src/main/java/com/pa/saitoo/services/ProdutoService.java
+++ src/main/java/com/pa/saitoo/services/ProdutoService.java
@@ -1,4 +1,39 @@
 package com.pa.saitoo.services;
 
+import com.pa.saitoo.models.ProdutoModel;
+import com.pa.saitoo.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoModel> findall(){
+        return produtoRepository.findAll();
+    }
+
+    public ProdutoModel cadastrarProduto(ProdutoModel produtoModel){
+        return produtoRepository.save(produtoModel);
+    }
+
+    public ProdutoModel listarPorId(Long id){
+        return produtoRepository.findById(id).get();
+    }
+
+    public ProdutoModel atualizarProduto(Long id, ProdutoModel produtoModel){
+        ProdutoModel produto = produtoRepository.findById(id).get();
+        produto.setName(produtoModel.getName());
+        produto.setPreco(produtoModel.getPreco());
+        produto.setQuantidade(produtoModel.getQuantidade());
+         return produtoRepository.save(produto);
+    }
+
+    public void removerProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
 }
